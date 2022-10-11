65fd482b1fbfa5f5e0a74aaeaa8689f687a8db87
Maximillian Brustkern <6314562+nuclearbob@users.noreply.github.com>
2022-10-11 14:51:19 -0400

    Add apt tests

--- os_validation_test.go
+++ os_validation_test.go
@@ -34,3 +34,15 @@ func TestYum(t *testing.T) {
 		run_cmd("yum", "-y", "install", "httpd")
 	})
 }
+
+func TestApt(t *testing.T) {
+	Convey("Apt should be able to update its cache files", t, func() {
+		run_cmd("bash", "DEBIAN_FRONTEND=noninteractive", "apt", "update")
+	})
+	Convey("Apt should be able to upgrade installed packages", t, func() {
+		run_cmd("bash", "apt", "-o", "Dpkg::Options::=\"--force-confold\"", "dist-upgrade", "-q", "-y", "--force-yes")
+	})
+	Convey("Apt should be able to install a new package", t, func() {
+		run_cmd("bash", "apt", "-o", "Dpkg::Options::=\"--force-confold\"", "install", "fortune-mod", "-q", "-y", "--force-yes")
+	})
+}
