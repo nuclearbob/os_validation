e530d358028a93bcf514269035a18f9c574d89a1
Maximillian Brustkern <6314562+nuclearbob@users.noreply.github.com>
2022-10-12 10:46:19 -0400

    Try revisions to apt tests

--- os_validation_test.go
+++ os_validation_test.go
@@ -2,6 +2,7 @@ package os_validation
 
 import (
 	"log"
+	"os"
 	"os/exec"
 	"testing"
 
@@ -11,6 +12,23 @@ import (
 func run_cmd(cmd string, args ...string) error {
 
 	command := exec.Command(cmd, args...)
+
+	err := command.Run()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	So(err, ShouldEqual, nil)
+
+	return err
+}
+
+func run_cmd_with_env(env string, cmd string, args ...string) error {
+
+	command := exec.Command(cmd, args...)
+	command.Env = os.Environ()
+	command.Env = append(command.Env, env)
 
 	err := command.Run()
 
@@ -36,13 +54,14 @@ func TestYum(t *testing.T) {
 }
 
 func TestApt(t *testing.T) {
+	// Doesn't work
 	Convey("Apt should be able to update its cache files", t, func() {
-		run_cmd("bash", "DEBIAN_FRONTEND=noninteractive", "apt", "update")
+		run_cmd_with_env("DEBIAN_FRONTEND=noninteractive", "apt", "update")
 	})
 	Convey("Apt should be able to upgrade installed packages", t, func() {
-		run_cmd("bash", "apt", "-o", "Dpkg::Options::=\"--force-confold\"", "dist-upgrade", "-q", "-y", "--force-yes")
+		run_cmd("apt", "-o", "Dpkg::Options::=\"--force-confold\"", "dist-upgrade", "-q", "-y", "--force-yes")
 	})
 	Convey("Apt should be able to install a new package", t, func() {
-		run_cmd("bash", "apt", "-o", "Dpkg::Options::=\"--force-confold\"", "install", "fortune-mod", "-q", "-y", "--force-yes")
+		run_cmd("apt", "-o", "Dpkg::Options::=\"--force-confold\"", "install", "fortune-mod", "-q", "-y", "--force-yes")
 	})
 }
