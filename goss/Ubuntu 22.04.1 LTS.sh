62f394e24b35228f76a87d5ba3acca27b83adaca
Maximillian Brustkern <6314562+nuclearbob@users.noreply.github.com>
2022-12-19 13:38:20 -0500

    Use go sysinfo name

---
+++ goss/Ubuntu 22.04.1 LTS.sh
@@ -0,0 +1,2 @@
+GOSSFILE="Ubuntu 22.04.1 LTS.yaml"
+goss --gossfile "$GOSSFILE" autoadd apt sshd
--- goss/ubuntu_22_04.sh
+++
@@ -1,3 +0,0 @@
-GOSSFILE=ubuntu_22_04.yaml
-goss --gossfile $GOSSFILE autoadd apt sshd
-ln -s $GOSSFILE "Ubuntu 22.04.1 LTS.yaml"
